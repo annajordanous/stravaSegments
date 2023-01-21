27fc046cb1982f12eee71b01812d2e682a86c595
annajordanous <annajordanous@hotmail.com>
2023-01-21 18:59:02 +0000

    been a while since I used this

--- segs.js
+++ segs.js
@@ -1,6 +1,9 @@
 var StravaApiV3 = require('strava_api_v3');
 var defaultClient = StravaApiV3.ApiClient.instance;
 
+var accessToken = {
+
+
 // Configure OAuth2 access token for authorization: strava_oauth
 var strava_oauth = defaultClient.authentications['strava_oauth'];
 strava_oauth.accessToken = "YOUR ACCESS TOKEN"
